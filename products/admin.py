5b6ba7148d2d9cd02abf096e85cffcee3e58f865
Renaldas Bendikas <renaldasb1999@gmail.com>
2022-12-14 12:10:49 +0000

    add template for products and create view for products

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import Category, Product
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name', 'name',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'rating', 'image',)
+
+    ordering = ('category',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
