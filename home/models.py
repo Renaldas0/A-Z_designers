b1cf90fbe90fd61b6c6f37391a2a418c4cf11977
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-01-12 15:19:37 +0000

    create customer model and signup view im home directory

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(max_length=100, default='')
+
+    def __str__(self):
+        return self.full_name
--- home/views.py
+++ home/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import Customer
 
 
 def index(request):
     """ A view to return the index page"""
     return render(request, 'home/index.html')
+
+
+""" Sign up Form """
+
+
+def sign_up(request):
+    if request.method == "POST":
+        form = Customer(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, index.html)
+    else:
+        form = Customer()
+        return render(request, 'signup.html', {'form': form})
