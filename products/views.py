7f442af519fc4758d1b907af14b46a8edaf2a910
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-01-15 20:43:10 +0000

    add product detail text file and create a view

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def all_products(request):
-    """ View that shows all products, including sorting and search queries """
+    """ View that shows ALL PRODUCTS, including sorting and search queries """
    
     products = Product.objects.all()
 
@@ -12,3 +12,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ View that shows INDIVIDUAL PRODUCTS """
+   
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
