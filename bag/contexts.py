7e60d67362155d6b88d66c52bf83a4e91c5578ee
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-01-23 20:27:06 +0000

    add context for bag items, create view for add_to_bag and link url in product-detail

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 """The purpose of this 'context processor' is to make this dictionary available to all templates across the whole application"""
 
@@ -9,6 +11,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_quantity = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_quantity += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
