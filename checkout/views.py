89329eeb2537cc9a9c5c81e53e09263a01fdada6
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-02-03 20:23:22 +0000

    test that payment intent is working and add a warning message if public key is missing

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         message.error(request, "Your bag is empty")
@@ -17,13 +20,22 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is not set!')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51MXUXPIEQO5wDvJsm0Us8BJCUnm55IrWyHOYagItPUXhuLebkwADxohdG3s7sb7efchPHPKFrbTiNx2h151FPjfO00i7QQl41f',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
