3743e4af10d83412bb3c34e426780e81b484c119
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-02-03 17:28:43 +0000

    add address form to checkout and link to secure checkout button

--- checkout/forms.py
+++ checkout/forms.py
@@ -26,7 +26,7 @@ class OrderForm(forms.ModelForm):
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
             else:
-                placeholder = placeholder[field]
+                placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
