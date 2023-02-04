3d4946f9731d7afd673b90cc6402424724dbf9b4
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-02-04 22:53:22 +0000

    add cache checkout data to urls , create a view and write the js required plus test that it works

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -17,13 +17,14 @@ class StripeWebHook_Handler:
     def handle_payment_intent_succeeded(self, event):
         # Handles the paymeny_intent.succeeded webhook from Stripe 
 
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
-        # Handles the paymeny_intent.failed webhook from Stripe 
-
+        # Handle the payment_intent.payment_failed webhook from Stripe
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
