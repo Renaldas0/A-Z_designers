ff0e8a9dc40cdb1dd1a38dfa6f24261e10f19773
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-02-10 13:28:00 +0000

    add the options to edit and delete products to product management, buttons added to product and product details and test that they work

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add_product/', views.add_product, name='add_product'),
     path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
-
+    path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
