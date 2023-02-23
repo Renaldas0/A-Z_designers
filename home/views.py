6d0399ce55c3d4fd369434c7081f503ce29191ef
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-02-23 22:53:57 +0000

    add terms and conditions, privacy policy and return policy templates, views and urls

--- home/views.py
+++ home/views.py
@@ -3,11 +3,11 @@ from .models import Customer
 
 
 def index(request):
-    """ A view to return the index page"""
+    # A view to return the index page
     return render(request, 'home/index.html')
 
 
-""" Sign up Form """
+# Sign up Form
 
 
 def sign_up(request):
@@ -21,7 +21,25 @@ def sign_up(request):
         return render(request, 'signup.html', {'form': form})
 
 
+def privacy_policy(request):
+    # A view to render the privacy policy page
+
+    return render(request, 'home/privacy_policy.html')
+
+
+def terms_and_conditions(request):
+    # A view to render the terms and conditions page
+
+    return render(request, 'home/terms_and_conditions.html')
+
+
+def return_and_refund_policy(request):
+    # A view to render the return and refund policy page
+
+    return render(request, 'home/return_and_refund_policy.html')
+
+
 def newsletter(request):
-    """ A view to return the newsletter page"""
+    # A view to return the newsletter page
     template = 'home/newsletter.html'
     return render(request, template)
