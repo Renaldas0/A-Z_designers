81215d2b497cca26799f97094de46cf5c46c6143
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-02-27 11:07:17 +0000

    correct error handler 500 typo in views.py main app

--- a_z_designers/views.py
+++ a_z_designers/views.py
@@ -16,6 +16,6 @@ def handler403(request, exception):
     return render(request, "errors/403.html", status=403)
 
 
-def handler404(request, exception):
+def handler500(request, exception):
     """ Error Handler 500 - Page Not Found """
     return render(request, "errors/500.html", status=500)
