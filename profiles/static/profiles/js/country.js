0841e2da9e99858e2c5a59fa2f631738b39e4504
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-02-27 10:04:44 +0000

    add a modal for delete product and delete review, add images to documentation

--- profiles/static/profiles/js/country.js
+++ profiles/static/profiles/js/country.js
@@ -1,9 +1,9 @@
 /* Sets the colour of country input placeholder to grey and if a country is selected to black */
 
-let countrySelected = $('#id_default_country')
+let countrySelected = $('#id_default_country');
 if (!countrySelected) {
     $('#id_default_country').css('color', '#aab7c4');
-};
+}
 
 $('#id_default_country').change(function () {
     countrySelected = $(this).val();
